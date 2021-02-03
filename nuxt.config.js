649f87e7be95c8f24e7d07bdc0389b94bf90ae86
Marat <marathovsepyan45@gmail.com>
2021-02-03 23:39:10 +0400

    gdfgdg

--- nuxt.config.js
+++ nuxt.config.js
@@ -22,7 +22,7 @@ export default {
 
   server: {
     port: 3000, // default: 3000
-    host: '0.0.0.0', // default: localhost,
+    host: 'https://nuxt-tes.herokuapp.com/', // default: localhost,
     timing: false
   },
 
